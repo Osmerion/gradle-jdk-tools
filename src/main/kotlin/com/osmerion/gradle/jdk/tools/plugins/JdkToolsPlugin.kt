dde0c79ea7df9ffefc97cedec9d7dddef5eee78c
Leon Linhart <themrmilchmann@gmail.com>
2024-11-28 23:17:24 +0100

    feat: add plugin class

---
+++ src/main/kotlin/com/osmerion/gradle/jdk/tools/plugins/JdkToolsPlugin.kt
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2024 Leon Linhart
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.osmerion.gradle.jdk.tools.plugins
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+/**
+ * The "entry-point" of the plugin.
+ *
+ * @since   0.1.0
+ *
+ * @author  Leon Linhart
+ */
+public class JdkToolsPlugin : Plugin<Project> {
+    // Does nothing because we only provide tasks
+    override fun apply(target: Project) {}
+}
