9ed84fa2ce81c07c530e7ce6f9f83b978782c06c
Leon Linhart <themrmilchmann@gmail.com>
2024-12-03 13:59:14 +0100

    chore: test configuration avoidance

---
+++ src/integrationTest/kotlin/com/osmerion/gradle/jdk/tools/plugins/JdkToolsPluginTest.kt
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2024 Leon Linhart
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.osmerion.gradle.jdk.tools.plugins
+
+import org.gradle.api.Project
+import org.gradle.testfixtures.ProjectBuilder
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.io.TempDir
+import java.io.File
+
+/**
+ * Tests for [JdkToolsPlugin].
+ *
+ * @author  Leon Linhart
+ */
+class JdkToolsPluginTest {
+
+    @TempDir
+    private lateinit var projectDir: File
+
+    private lateinit var project: Project
+
+    @BeforeEach
+    fun setup() {
+        project = ProjectBuilder.builder()
+            .withProjectDir(projectDir)
+            .build()
+    }
+
+    @Test // Test that no tasks are added to the project
+    fun testConfigurationAvoidance() {
+        val taskCount = project.tasks.size
+        project.pluginManager.apply(JdkToolsPlugin::class.java)
+        assertEquals(taskCount, project.tasks.size)
+    }
+
+}
